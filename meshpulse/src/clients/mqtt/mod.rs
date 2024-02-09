b5e93556c7627b29d6e5c5b57b522fa7f547ec0d
Thomas Luijken <thomas@luijken.dev>
2024-02-09 12:04:33 +0100

    Initial implementation for mqtt Events

---
+++ meshpulse/src/clients/mqtt/mod.rs
@@ -0,0 +1,97 @@
+use std::error::Error;
+
+pub trait Publish {
+    // publish a message to a topic
+    fn publish(&self) -> Result<(), Box<dyn Error>>;
+}
+
+pub trait Subscribe {
+    // define a subscribe function that takes in a callback, which can be called from a different
+    // thread
+    type Event;
+    fn subscribe(callback: impl FnMut(Self::Event) -> () + Send + 'static) -> Result<Subscription, Box<dyn Error>>;
+}
+
+// prelude
+pub mod prelude {
+    pub use super::Publish;
+    pub use super::Subscribe;
+}
+
+// create a generic subscription struct for type T which can hold the subscription thread alive
+pub struct Subscription {
+    _thread: std::thread::JoinHandle<()>,
+    topic: String,
+    connection: paho_mqtt::Client,
+}
+
+impl Subscription {
+    pub fn unsubscribe(self) -> Result<(), Box<dyn Error>> {
+        self.connection.unsubscribe(&self.topic).unwrap();
+        self.connection.disconnect(None).unwrap();
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::{Arc, Mutex};
+
+    // create a client that actually connects to a broker
+    use super::*;
+    use serde::{Serialize, Deserialize};
+    use meshpulse_derive::Event;
+
+    fn setup_enviroment_variables() {
+        std::env::set_var("MQTT_USERNAME", "test");
+        std::env::set_var("MQTT_PASSWORD", "test");
+        std::env::set_var("MQTT_HOST", "tcp://localhost:1883");
+    }
+
+    #[derive(Serialize,Deserialize, Event)]
+    struct TestEvent {
+        message: String,
+    }
+
+    #[test]
+    fn test_publish() {
+        setup_enviroment_variables();
+        let event = TestEvent {
+            message: "hello".to_string(),
+        };
+        let result = event.publish();
+        assert_eq!(result.is_ok(), true);
+    }
+
+    #[test]
+    fn test_subscribe() {
+        setup_enviroment_variables();
+        // Setup: Prepare a shared counter for event reception
+        let received = Arc::new(Mutex::new(0));
+
+        // Action: Subscribe to TestEvent and publish an event
+        let sub_result = TestEvent::subscribe({
+            let received_clone = Arc::clone(&received);
+            move |event: TestEvent| {
+                if event.message == "World" {
+                    let mut count = received_clone.lock().unwrap();
+                    *count += 1;
+                    println!("Received event: {:?}", event.message);
+                }
+            }
+        });
+
+        assert!(sub_result.is_ok(), "Subscription should succeed");
+
+        // Simulate event publication
+        TestEvent { message: "World".to_string() }.publish().expect("Event publication failed");
+
+        // Cleanup: Unsubscribe if necessary and perform assertions
+        let unsub_result = sub_result.unwrap().unsubscribe();
+        assert!(unsub_result.is_ok(), "Unsubscription should succeed");
+
+        // Assertion: Verify that the event was received
+        let received_count = *received.lock().unwrap();
+        assert_eq!(received_count, 1, "Expected exactly 1 event to be received");
+    }
+}
---
+++ meshpulse_derive/src/lib.rs
@@ -0,0 +1,79 @@
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+
+#[cfg(feature = "mqtt")]
+#[proc_macro_derive(Event, attributes(Event))]
+pub fn event_macro(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let struct_name = &input.ident;
+
+    let expanded = quote! {
+        fn get_env_var(name: &str) -> String {
+            match std::env::var(name) {
+                Ok(val) => val,
+                Err(e) => panic!("{} is not set: {}", name, e),
+            }
+        }
+        
+        fn getClient() -> paho_mqtt::Client {
+            let options = paho_mqtt::ConnectOptionsBuilder::new()
+                .user_name(get_env_var("MQTT_USERNAME"))
+                .password(get_env_var("MQTT_PASSWORD"))
+                .finalize();
+            let opt_opts = paho_mqtt::CreateOptionsBuilder::new()
+                .server_uri(get_env_var("MQTT_HOST"))
+                .finalize();
+            let client = paho_mqtt::Client::new(opt_opts).unwrap();
+            client.connect(options).expect("Failed to connect");
+            client
+
+        }
+
+        impl Publish for #struct_name {
+            fn publish(&self) -> Result<(), Box<dyn Error>> {
+                // the topic is the name of the struct, use reflection to get it
+                let topic = format!("events/{}", std::any::type_name::<Self>());
+                let payload = serde_json::to_string(&self).unwrap();
+
+                let msg = paho_mqtt::Message::new(topic, payload , 0);
+                getClient().publish(msg).unwrap();
+                Ok(())
+            }
+        }
+
+        impl Subscribe for #struct_name {
+            type Event = Self;
+            fn subscribe(mut callback: impl FnMut(Self) -> () + Send + 'static) -> Result<Subscription, Box<dyn Error>> {
+                let cli = getClient();
+                // the topic is the name of the struct, use reflection to get it
+                let topic = format!("events/{}", std::any::type_name::<Self>());
+                // loop and call the callback
+                let rx = cli.start_consuming();
+                cli.subscribe(&topic, 0).unwrap();
+                let thread = std::thread::spawn(move || {
+                    for msg in rx.iter() {
+                        match msg {
+                            Some(msg) => {
+                                let payload = msg.payload_str().to_string();
+                                let event: TestEvent = serde_json::from_str(&payload).unwrap();
+                                callback(event);
+                            }
+                            None => {}
+                        }
+                    }
+                });
+                let sub = Subscription {
+                    _thread: thread,
+                    connection: cli,
+                    topic,
+                };
+
+                Ok(sub)
+            }
+        }
+    };
+    TokenStream::from(expanded)
+}
